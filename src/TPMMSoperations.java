d0ef7527cc8383fca2b0fbf30d53311ace0657dc
An Nguyen <nnthienan1994@gmail.com>
2020-02-12 16:19:15 -0500

    Phase 2

--- src/TPMMSoperations.java
+++ src/TPMMSoperations.java
@@ -6,10 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 
 
@@ -20,7 +25,7 @@ public class TPMMSoperations {
 	        phase1(folder);
 	    }
 
-	private void phase1(File folder) throws IOException {
+	private void phase1(File folder) throws IOException, ParseException {
 		
 		File[] fileNames = folder.listFiles();
 		//System.out.println(fileNames.length);
@@ -59,8 +64,9 @@ public class TPMMSoperations {
 	
 	private File generateSublist(String name, String[] records1) throws IOException {
 	///	System.out.println("Size---->:"+records1.length);
+		
 		   File temp = File
-	                .createTempFile(name, null, new File("C://Twilight Series"));
+	                .createTempFile(name, null, new File(System.getProperty("user.dir")));
 	        OutputStream outputStream = new FileOutputStream(temp);
 	        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
 	        	
@@ -82,9 +88,64 @@ public class TPMMSoperations {
 	}
 	
     private void phase2(List<File> fileList)
-            throws IOException {
+            throws IOException, ParseException {
     	
-    }
+    	String dateFormat = "yyyy-MM-dd";
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+    	List<File> tmp = new ArrayList<>();
+    	int number_line_to_read=(calculateAvailableMemory()-(1024*1024)) / (fileList.size());
+    	System.out.println(number_line_to_read);
+    	TreeMap<String, String> records = new TreeMap<>();
+//    	int count = 0;
+//    	for(File file : fileList) {
+//    		BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()));
+//    		String tuple = br.readLine();
+//    		while(tuple != null) {
+//    			if (count>=number_line_to_read) {
+//  	              
+//                	count=0;
+//                	 tmp.add(generateSublist(file.getName(),records));
+//                	records= new TreeMap<>();
+//                }
+//			  if (!tuple.isEmpty()) {
+//	                
+//				  if(count==0) {
+//					  records= new TreeMap<>();
+//				  }
+//				  if(!records.containsKey(tuple.substring(0, 8))) {
+//						records.put(tuple.substring(0, 8), tuple);
+//					}else {
+//						Date dateNew = sdf.parse(tuple.substring(8, 18));
+//						Date dateOld = sdf.parse(records.get(tuple.substring(0, 8)).substring(8, 18));
+//						if(dateNew.after(dateOld)) {
+//							records.put(tuple.substring(0, 8), tuple);
+//						}
+//					}
+//				//  System.out.println(tuple);
+//	                }
+//			  count=count+1;
+//			  tuple = br.readLine();
+//    		}
+//    		br.close();
+    	}
+
+	private File generateSublist(String name, TreeMap<String, String> records) throws IOException {
+		File temp = File
+                .createTempFile(name, null, new File("src/mergeTemp/"));
+        OutputStream outputStream = new FileOutputStream(temp);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+        	
+        for(Entry<String, String> record : records.entrySet()) {
+        writer.write(record.getValue());
+		writer.newLine();
+        }
+        writer.close();
+        
+        outputStream.close();
+   records=null;
+        
+        return temp;
+	}
 
 	private int calculateAvailableMemory() {
 		System.gc();
